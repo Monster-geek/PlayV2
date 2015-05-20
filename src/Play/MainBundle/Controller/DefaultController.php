7e1c01e549a149cd7bf1466bbc41d3b413b6df62
Alexandre DIDIER <alexlegrem@gmail.com>
2015-05-20 04:07:46 +0200

    Add some basic stuff like :
    - Bootstrap framework
    - Jquery lib
    - Background image
    - Login form

---
+++ src/Play/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Play\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('PlayMainBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Play/MainBundle/PlayMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Play\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PlayMainBundle extends Bundle
+{
+}
