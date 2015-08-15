3e3e58526d4ed1b81d7cf3d95ecfca0aab445716
Alexandre DIDIER <alexlegrem@gmail.com>
2015-08-15 03:02:33 +0200

    Pre-build the template for Auction House system.

--- src/Play/MainBundle/Controller/AHController.php
+++ src/Play/MainBundle/Controller/AHController.php
@@ -4,13 +4,14 @@ namespace Play\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class SessionController extends Controller {
+class AHController extends Controller
+{
 
-    public function homeAction()
+    public function displayAction()
     {
         $current_user = $this->get('security.context')->getToken()->getUser();
 
-        return $this->render('PlayMainBundle:game-core:home.html.twig', array('user'=>$current_user));
+        return $this->render('PlayMainBundle:game-core:AuctionHouse.html.twig', array('user'=>$current_user));
     }
 
 } 
